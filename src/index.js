fbb199fda641e396d7ce5c0efd382b42fe2dfa17
bogdanslepov <6ordan@gmail.com>
2020-08-25 00:04:49 +0300

    other commit

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = this.state.squares.slice();
+        const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
@@ -103,7 +103,7 @@ class Game extends React.Component {
 
         let status;
         if (winner) {
-            status = 'Won' + winner;
+            status = 'Won ' + winner;
         } else {
             status = 'Next move ' + (this.state.xIsNext ? 'X' : 'O');
         }
